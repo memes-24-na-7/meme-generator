e04d216e110f8795bfa8c46acc8154fcd188bcf8
Julia <juliachirkova@e1.ru>
2022-05-13 15:44:52 +0500

    changed structure and style

--- public/scripts/1.js
+++ public/scripts/1.js
@@ -1,61 +1,38 @@
 // import {writeOnImage} from "./editingImages";
 // import html2canvas from './html2canvas.js';
 
-
+/*#region Обработка нажатия кнопок*/
 var generate_pressed = function () {
+    document.getElementById('generate_b').style.display = 'none';
+    document.getElementById('upload_b').style.display = 'none';
+    document.getElementById('div_for_images').style.display = 'block';
+    document.getElementById('div_for_text_editing').style.display = 'block';
     document.getElementById('download_b').style.display = 'block';
     document.getElementById('back_b').style.display = 'block';
-    document.getElementById('generate_b').style.display = 'none';
-    document.getElementById('mem_image').style.display = 'block';
-    draw_image();
+    draggable = document.getElementById('draggable');
+    draggable.style.top = '-360px';
+    draggable.style.left = '0px';
+    //TODO: сделать чтоб обновлялось начальное положение текста
+}
 
-    var text = document.getElementById('dragable_text');
-    text.style.display = 'block';
-    text.onmousedown = function(event) {
-        let shiftX = event.clientX - text.getBoundingClientRect().left;
-        let shiftY = event.clientY - text.getBoundingClientRect().top;
-        text.style.position = 'absolute';
-        text.style.zIndex = 1000;
-        document.body.append(text);
-        moveAt(event.pageX, event.pageY);
-        function moveAt(pageX, pageY) {
-            text.style.left = pageX - shiftX + 'px';
-            text.style.top = pageY - shiftY + 'px';
-        }
-        function onMouseMove(event) {
-            moveAt(event.pageX, event.pageY);
-        }
-        document.addEventListener('mousemove', onMouseMove);
-        text.onmouseup = function() {
-            document.removeEventListener('mousemove', onMouseMove);
-            text.onmouseup = null;
-        };
-    };
-    text.ondragstart = function() {
-        return false;
-    };
-}
-var draw_image = function () {
-    kartinochka = document.getElementById('mem_image');
-    kartinochka.src = '../public/images/Doggies.jpg';
-    document.body.appendChild(kartinochka);
-}
-// var download_pressed = function () {
-//     kartinochka = document.getElementById('mem_image');
-//
-//     let canvas = document.getElementById('meme');
-//     let link = document.createElement("a");
-//     link.setAttribute("href", kartinochka.src);
-//     link.setAttribute("download", `${Date.now()}`);
-//     link.click();
-// }
+var upload_pressed = function () { }
+
 var back_pressed = function () {
+    document.getElementById('div_for_images').style.display = 'none';
+    document.getElementById('div_for_text_editing').style.display = 'none';
     document.getElementById('download_b').style.display = 'none';
     document.getElementById('back_b').style.display = 'none';
     document.getElementById('generate_b').style.display = 'block';
-    document.getElementById('mem_image').style.display = 'none';
-    document.getElementById('dragable_text').style.display = 'none';
+    document.getElementById('upload_b').style.display = 'block';
 }
+/*#endregion*/
+
+// var draw_image = function () {
+//     kartinochka = document.getElementById('mem_image');
+//     kartinochka.src = '../public/images/Doggies_reserve.jpg';
+//     document.body.appendChild(kartinochka);
+// }
+
 var connectParts = function () {
     let meme = document.createElement('div');
     meme.style.display = 'inline-block';
@@ -81,3 +58,54 @@ var connectParts = function () {
     meme.appendChild(textPic);
     return meme;
 }
+
+
+/*#region Для передвижения и изменения размера контейнера текста*/
+var x, y, target = null;
+
+document.addEventListener('mousedown', function(e) {
+    fit_text_box_size();
+    for (var i = 0; e.path[i] !== document.body; i++) {
+        if (e.path[i].classList.contains('draggable')) {
+            target = e.path[i];
+            if (target.style.left === '' || target.style.top === '') {
+                target.style.left = 0 + 'px'; // место клика на экране
+                target.style.top = -360 + 'px';
+            }
+            target.classList.add('dragging');
+            x = e.clientX - target.style.left.slice(0, -2); // место клика на экране
+            y = e.clientY - target.style.top.slice(0, -2);
+            return;
+        }
+    }
+});
+
+var fit_text_box_size = function () {
+    var text_div = document.getElementById('draggable');
+    var text_image = document.getElementById('text_image');
+    var new_w = Math.round(text_image.getBoundingClientRect().width) + 'px';
+    var new_h = Math.round(text_image.getBoundingClientRect().height) + 'px';
+    text_div.style.width = new_w;
+    text_div.style.height = new_h;
+}
+
+var counter = 0;
+
+document.addEventListener('mouseup', function() {
+    if (target !== null) target.classList.remove('dragging');
+    target = null;
+    counter += 1;
+});
+document.addEventListener('mousemove', function(e) {
+    if (target === null) return;
+    target.style.left = e.clientX - x + 'px';
+    target.style.top = e.clientY - y + 'px';
+    var pRect = target.parentElement.getBoundingClientRect();
+    var tgtRect = target.getBoundingClientRect();
+    if (tgtRect.left < pRect.left) target.style.left = 0;
+    if (tgtRect.top < pRect.top) target.style.top = -360+'px';
+    if (tgtRect.right > pRect.right) target.style.left = pRect.width - tgtRect.width + 'px';
+    if (tgtRect.bottom > pRect.bottom) target.style.top = pRect.height - tgtRect.height -360 + 'px';
+});
+/*#endregion*/
+
