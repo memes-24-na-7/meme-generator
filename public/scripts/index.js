d5ac3ff59d113465bb06a8a2f71e7363566be683
Julia <juliachirkova@e1.ru>
2022-05-22 12:13:21 +0500

    Fixed text behavior

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,87 @@
 /*#region Обработка нажатия кнопок*/
 let imageType = '';
+let editorMode = false;
 
 let generatePressed = function () {
     imageType = 'image/png';
-    let number = getRandomInt(7) + 1;
-    launchEditor(`../public/images/${number}.png`);
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(result => {
+            let randNumber = getRandomInt(100);
+            launchWithImageUrl(result.data.memes[randNumber].url);
+        })
+        .catch(err => console.log(err));
 };
 
+// TODO: сброс положения текста
+let textToStartPosition = function () {
+    let textDiv = document.getElementById('draggable');
+    let textImage = document.getElementById('text-image');
+    let newW = Math.round(textImage.getBoundingClientRect().width);
+    let newH = Math.round(textImage.getBoundingClientRect().height);
+    textDiv.style.width = newW + 'px'; // TODO: почему-то не сразу действует
+    textDiv.style.height = newH + 'px';
+    textDiv.style.left = 0;
+    textDiv.style.top = -document.getElementById('div-for-images').getBoundingClientRect().height + 5 + "px";
+    document.getElementById('draggable').click();
+}
+
 let backPressed = function () {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    document.getElementById('generate-btn').style.display = 'none';
     document.querySelectorAll('.second-state').forEach(function (elem) {
-            elem.style.visibility = 'hidden';
-        });
+        elem.style.visibility = 'hidden';
+    });
     document.querySelectorAll('.first-state').forEach(function (elem) {
-            elem.style.visibility = 'visible';
-        });
+        elem.style.visibility = 'visible';
+    });
+    document.getElementById('text-image').src = '../public/images/logo.png';
+    document.getElementById("text-input").value = '';
+    document.getElementById("font-select").value = 'Tahoma';
+    document.getElementById("size-input").value = 100;
+    editorMode = false;
 };
+
+let downloadImgToGallery = function() {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(result => {
+            let counter = parseFloat(document.getElementById('counter').textContent);
+            for(i = counter * 10; i < counter * 10 + 10; i++)
+            {
+                let newImageDiv = document.createElement('div');
+                newImageDiv.className = "image";
+                let img = document.createElement('img');
+                img.src = result.data.memes[i].url;
+                //img.id = `img${i}`;
+                //document.getElementById(`img${i}`).onclick = chooseImage(img);
+                img.setAttribute('onclick', 'chooseImage(this)');
+                img.style.width = "100%";
+                newImageDiv.appendChild(img);
+                //newImageDiv.innerHTML += `<img src=${result.data.memes[i].url} onclick="chooseImage(this);" style="width:100%">`;
+                // для картинок локально (for(i = 1; i <= 7; i++))
+                //board.innerHTML += `<img src='../public/images/${x}.png' onclick="chooseImage(this);" style="width:100%">`;
+                document.getElementsByClassName('modal-body')[0].appendChild(newImageDiv);
+            }
+            document.getElementById('counter').textContent = (counter + 1).toString();
+        })
+        .catch(err => console.log(err));
+};
+
+let launchWithImageUrl = function(url) {
+    const img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    img.onload = () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        const dataURL = canvas.toDataURL("image/png");
+        loadSrcToEdit(dataURL);
+    }
+    img.src = url
+}
 
 let getRandomInt = function (max) {
     return Math.floor(Math.random() * max);
@@ -32,7 +99,7 @@ let uploadMeme = function (file) {
 
     imageType = file.type;
     let reader = new FileReader();
-    reader.onloadend = () => launchEditor(reader.result);
+    reader.onloadend = () => loadSrcToEdit(reader.result);
     reader.readAsDataURL(file);
 };
 
@@ -44,39 +111,70 @@ let chooseImage = function (imgs) {
     let modal = document.getElementById('myModal');
     modal.style.display = "none";
     imageType = 'image/png';
-    launchEditor(imgs.src);
+    launchWithImageUrl(imgs.src);
 };
 
 let resizeEditorWindows = function (width, height) {
     let memeImage = document.getElementById('mem-image');
     memeImage.style.width = width;
     memeImage.style.height = height;
-    document.getElementById('app').style.width = width;
+    document.getElementById('meme-container').style.height = height;
+    document.getElementById('text-generator-form').style.width = width;
     document.querySelectorAll('.editor-block').forEach(function (elem) {
         elem.style.width = width;
-        elem.style.height = height;
-        // TODO: При "широком" положении не надо менять размер текстового дива, он должен быть постоянным
     });
-    // document.getElementById('app')
+    document.getElementById('div-for-images').style.height = height;
     let draggable = document.getElementById('draggable');
-    draggable.style.top = '-' + height;
+    draggable.style.top = '-' + height.slice(0, -2) - 5 + "px"; // TODO:
     draggable.style.left = '0px';
 };
+
+let launchEditorPage = function () {
+    document.getElementById('generate-btn').style.display = 'block';
+    document.querySelectorAll('.first-state').forEach(function (elem) {
+        elem.style.visibility = 'hidden';
+    });
+    document.querySelectorAll('.second-state').forEach(function (elem) {
+        elem.style.visibility = 'visible';
+    });
+}
 
 let adaptImgSize = function() {
     let memeWidth = this.width;
     let memeHeight = this.height;
-    let maxWidth = document.body.clientWidth * 0.9;
+    let minWidth = 350;
+    let minHeight = 75;
+    let maxWidth = window.screen.width * 0.9;
+    let maxHeight = window.screen.height * 0.65;
+
+    if (memeWidth < minWidth) {
+        memeHeight = minWidth * memeHeight / memeWidth;
+        if (memeHeight > maxHeight) {
+            alert("Your image is too narrow, crop it or choose another one.");
+            return;
+        }
+        memeWidth = minWidth;
+    }
+
+    if (memeHeight < minHeight) {
+        memeWidth = minHeight * memeWidth / memeHeight;
+        if (memeWidth > maxWidth) {
+            alert("Your image is too wide, crop it or choose another one.");
+            return;
+        }
+        memeHeight = minHeight;
+    }
+
     if (memeWidth > maxWidth) {
         memeHeight = maxWidth * memeHeight / memeWidth;
         memeWidth = maxWidth;
     }
-    let maxHeight = document.body.clientHeight * 0.65;
     if (memeHeight > maxHeight) {
         memeWidth = maxHeight * memeWidth / memeHeight;
         memeHeight = maxHeight;
     }
     resizeEditorWindows(String(memeWidth) + 'px', String(memeHeight) + 'px');
+    launchEditorPage();
 };
 
 let checkImgSize = function (img) {
@@ -84,18 +182,12 @@ let checkImgSize = function (img) {
     return !(img.width > size || img.height > size);
 };
 
-let launchEditor = function (imgSrc) {
+let loadSrcToEdit = function (imgSrc) {
     let img = new Image();
     img.src = imgSrc;
     if (checkImgSize(img)) {
         document.getElementById('mem-image').src = imgSrc;
         img.onload = adaptImgSize;
-        document.querySelectorAll('.first-state').forEach(function (elem) {
-            elem.style.visibility = 'hidden';
-        });
-        document.querySelectorAll('.second-state').forEach(function (elem) {
-            elem.style.visibility = 'visible';
-        });
     }
 };
 /*#endregion*/
@@ -128,7 +220,7 @@ let fitTextBoxSize = function () {
     let newH = Math.round(textImage.getBoundingClientRect().height) + 'px';
     textDiv.style.width = newW;
     textDiv.style.height = newH;
-}
+} //
 
 let counter = 0;
 
@@ -150,13 +242,13 @@ document.addEventListener('mousemove', function(e) {
         target.style.left = 0;
     }
     if (tgtRect.top < pRect.top) {
-        target.style.top = -Math.round(divForImagesHeight) + 'px';
+        target.style.top = -divForImagesHeight + 5 + 'px'; // TODO:
     }
     if (tgtRect.right > pRect.right) {
         target.style.left = pRect.width - tgtRect.width + 'px';
     }
     if (tgtRect.bottom > pRect.bottom) {
-        target.style.top = pRect.height - tgtRect.height - Math.round(divForImagesHeight) + 'px';
+        target.style.top = pRect.height - tgtRect.height - divForImagesHeight + 6 + 'px'; // TODO:
     }
 });
 /*#endregion*/
@@ -181,6 +273,7 @@ const btn = document.getElementById("generate-btn");
 btn.addEventListener("click", async () => {
     btn.setAttribute("disabled", "true");
     await generateImage();
+    textToStartPosition(); // TODO:
     btn.removeAttribute("disabled");
 });
 
